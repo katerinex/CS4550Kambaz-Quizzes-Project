8c81175870f88ac3a48c2b29b39153e1a59ed7e9
katerinex <145131693+katerinex@users.noreply.github.com>
2025-03-12 23:14:53 -0400

    commit

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,11 @@
 // src/Labs/index.tsx
 import React from 'react';
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom"; // Import useLocation
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-import Lab4 from "./Lab4"; 
-import Lab5 from "./Lab5"; 
+import Lab4 from "./Lab4";
+import Lab5 from "./Lab5";
 import TOC from "./TOC";
 import store from "./store";
 import { Provider } from "react-redux";
@@ -22,8 +22,8 @@ export default function Labs() {
           <Route path="Lab1" element={<Lab1 />} />
           <Route path="Lab2/*" element={<Lab2 />} />
           <Route path="Lab3/*" element={<Lab3 />} />
-          <Route path="Lab4" element={<Lab4 />} /> 
-          <Route path="Lab5" element={<Lab5 />} /> 
+          <Route path="Lab4" element={<Lab4 />} />
+          <Route path="Lab5" element={<Lab5 />} />
         </Routes>
       </div>
     </Provider>
@@ -35,8 +35,13 @@ interface NavigateToLab1Props {
 }
 
 const NavigateToLab1: React.FC<NavigateToLab1Props> = ({ navigate }) => {
+  const location = useLocation(); // Get the current location
+
   React.useEffect(() => {
-    navigate("Lab1");
-  }, [navigate]);
+    if (location.pathname === "/") { // Check if the path is exactly "/"
+      navigate("Lab1");
+    }
+  }, [navigate, location.pathname]); // Add location.pathname to dependencies
+
   return null;
 };
